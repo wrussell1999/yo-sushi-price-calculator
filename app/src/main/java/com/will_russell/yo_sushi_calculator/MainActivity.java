483d2543917a10f8fe79babd5334d99340edd3ad
wrussell1999 <jetpowerss@gmail.com>
2019-02-18 03:26:38 +0000

    Refactor

--- app/src/main/java/com/will_russell/yo_sushi_calculator/MainActivity.java
+++ app/src/main/java/com/will_russell/yo_sushi_calculator/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     TextView pink_tv;
     TextView grey_tv;
     TextView price_tv;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
