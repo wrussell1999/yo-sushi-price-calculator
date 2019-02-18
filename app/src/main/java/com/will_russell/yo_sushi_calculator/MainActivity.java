b4e920a83f04f371cc93d8c4f57a8987f3d7246c
wrussell1999 <jetpowerss@gmail.com>
2019-02-18 02:38:09 +0000

    Buttons now increment total

--- app/src/main/java/com/will_russell/yo_sushi_calculator/MainActivity.java
+++ app/src/main/java/com/will_russell/yo_sushi_calculator/MainActivity.java
@@ -3,12 +3,96 @@ package com.will_russell.yo_sushi_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView green_tv;
+    TextView blue_tv;
+    TextView purple_tv;
+    TextView orange_tv;
+    TextView pink_tv;
+    TextView grey_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        green_tv = (TextView) findViewById(R.id.green_total);
+        blue_tv = (TextView) findViewById(R.id.blue_total);
+        purple_tv = (TextView) findViewById(R.id.purple_total);
+        orange_tv = (TextView) findViewById(R.id.orange_total);
+        pink_tv = (TextView) findViewById(R.id.pink_total);
+        grey_tv = (TextView) findViewById(R.id.grey_total);
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.green_left_button:
+                remove(green_tv);
+                break;
+            case R.id.blue_left_button:
+                remove(blue_tv);
+                break;
+            case R.id.purple_left_button:
+                remove(purple_tv);
+                break;
+            case R.id.orange_left_button:
+                remove(orange_tv);
+                break;
+            case R.id.pink_left_button:
+                remove(pink_tv);
+                break;
+            case R.id.grey_left_button:
+                remove(grey_tv);
+                break;
+            case R.id.green_right_button:
+                add(green_tv);
+                break;
+            case R.id.blue_right_button:
+                add(blue_tv);
+                break;
+            case R.id.purple_right_button:
+                add(purple_tv);
+                break;
+            case R.id.orange_right_button:
+                add(orange_tv);
+                break;
+            case R.id.pink_right_button:
+                add(pink_tv);
+                break;
+            case R.id.grey_right_button:
+                add(grey_tv);
+                break;
+        }
+    }
+
+    public void add(TextView tv) {
+        System.out.println("Adding");
+        String text = tv.getText().toString();
+        int value = Integer.parseInt(text);
+        value++;
+        tv.setText(String.valueOf(value));
+    }
+
+    public void remove(TextView tv) {
+        System.out.println("Removing");
+        String text = tv.getText().toString();
+        int value = Integer.parseInt(text);
+        if (value != 0) {
+            value--;
+            tv.setText(String.valueOf(value));
+        }
+    }
+
+    public void clear(View v) {
+        green_tv.setText("0");
+        blue_tv.setText("0");
+        purple_tv.setText("0");
+        orange_tv.setText("0");
+        pink_tv.setText("0");
+        grey_tv.setText("0");
     }
 }
